fece5967404e3073cb44164afa987a02b29fa72c
nuwan sanjeewa abeysiriwardana <nuwan.a@ustocktrade.com>
2017-01-20 16:46:57 +0530

    add missing entity exception

---
+++ spi/src/main/java/com/ust/spi/ex/EntityException.java
@@ -0,0 +1,12 @@
+package com.ust.spi.ex;
+
+public class EntityException extends RuntimeException {
+  
+  public EntityException(Exception ex) {
+    super(ex);
+  }
+  
+  public EntityException(String message) {
+    super(message);
+  }
+}
