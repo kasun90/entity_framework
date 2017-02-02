e415e21d98b325878965120eb58e8fd2eadd53c1
nuwan sanjeewa abeysiriwardana <nuwan.a@ustocktrade.com>
2017-02-02 13:48:08 +0530

    remove unused import

--- spi/src/test/java/com/ust/spi/test/entity/TestMapEntity.java
+++ spi/src/test/java/com/ust/spi/test/entity/TestMapEntity.java
@@ -1,10 +1,7 @@
 package com.ust.spi.test.entity;
 
 import com.ust.spi.MapEntity;
-import com.ust.spi.ex.EntityException;
 import com.ust.spi.test.EntityItem;
-import com.ust.spi.test.event.EntityExceptionCreationEvent;
-import com.ust.spi.test.event.PasswordChanged;
 import com.ust.spi.test.event.UserCreated;
 
 @SuppressWarnings("unused")
